8d5dd06e338ae64611f6750f473dcd3e1464c561
gaetanovenuto <gaetanovenuto99@live.it>
2025-01-23 14:51:19 +0100

    Creato carosello automatico nel footer

--- src/components/Footer/AppFooter.vue
+++ src/components/Footer/AppFooter.vue
@@ -18,9 +18,21 @@ export default {
 
 <template>
     <div class="reviews bg-darkBeige text-white">
-        <h1 class="words-about-us text-center d-flex justify-content-evenly align-items-center py-5">
+        <h1 class="words-about-us text-center d-flex justify-content-evenly align-items-center pt-5 pb-1">
             DICONO DI NOI <i class="fa-solid fa-diamond fs-4"></i> DICONO DI NOI
         </h1>
+        <div class="carousel-container d-flex align-items-center">
+            <div class="carousel-slide d-flex align-items-center">
+                <div v-for="(phrase, index) in loremPhrases" :key="index" class="text-white w-100 d-flex align-items-center mt-2">
+                    {{ phrase }} <i class="fa-solid fa-diamond diamond-icon mx-2"></i>
+                </div>
+            </div>
+            <div class="carousel-slide d-flex align-items-center">
+                <div v-for="(phrase, index) in loremPhrases" :key="index" class="text-white w-100 d-flex align-items-center mt-2">
+                    {{ phrase }} <i class="fa-solid fa-diamond diamond-icon mx-2"></i>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -33,5 +45,29 @@ export default {
         font-size: 115px;
         font-weight: 300;
     }
+
+    .carousel-container {
+        overflow: hidden;
+        white-space: nowrap;
+
+        .carousel-slide {
+            display: inline-block;
+            animation: 15s slide infinite linear;
+
+            .diamond-icon {
+                font-size: 8px;
+            }
+        }
+    }
+
+    @keyframes slide {
+        from {
+            transform: translateX(0);
+        }
+        to {
+            transform: translateX(-100%);
+        }
+    }
+
 }
 </style>
